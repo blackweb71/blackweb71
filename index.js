44e74195ee2ba8b7279d0a710ba39c1e122550c4
MD Rajib <83833885+blackweb71@users.noreply.github.com>
2021-11-18 12:40:40 +0600

    Update index.js

--- index.js
+++ index.js
@@ -2,10 +2,8 @@ x = document.querySelector("title");
 a = document.querySelector(".but1");
 b = document.querySelector(".but2");
 c = document.querySelector(".but3");
-d = document.querySelector(".but4");
 e = document.querySelector(".but5");
 f = document.querySelector(".but6");
-g = document.querySelector(".but7");
 h = document.querySelector(".but8");
 
 
@@ -13,9 +11,7 @@ x.innerHTML="Villain";
 a.innerHTML="Bangla";
 b.innerHTML="Bengali";
 c.innerHTML="Bollywood";
-d.innerHTML="Hindi Dubbed";
 e.innerHTML="Hollywood";
 f.innerHTML="Tamil";
-g.innerHTML="Drive";
 h.innerHTML="Movie Requests";
 
