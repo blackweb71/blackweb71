b1435895ca60a69d92ef09bfe22ac6758ea5c6ac
MD Rajib <83833885+blackweb71@users.noreply.github.com>
2021-11-18 12:43:12 +0600

    Update index.js

--- index.js
+++ index.js
@@ -2,16 +2,16 @@ x = document.querySelector("title");
 a = document.querySelector(".but1");
 b = document.querySelector(".but2");
 c = document.querySelector(".but3");
-e = document.querySelector(".but5");
-f = document.querySelector(".but6");
-h = document.querySelector(".but8");
+d = document.querySelector(".but4");
+f = document.querySelector(".but5");
+g = document.querySelector(".but6");
 
 
 x.innerHTML="Villain";
 a.innerHTML="Bangla";
 b.innerHTML="Bengali";
 c.innerHTML="Bollywood";
-e.innerHTML="Hollywood";
+d.innerHTML="Hollywood";
 f.innerHTML="Tamil";
-h.innerHTML="Movie Requests";
+g.innerHTML="Movie Requests";
 
